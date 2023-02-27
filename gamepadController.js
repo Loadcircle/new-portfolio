f12bce5442c97e42150d152cc3dc823e42a4bcd9
Jesus <jesusmilano96@gmail.com>
2023-02-26 21:38:02 -0500

    .

--- gamepadController.js
+++ gamepadController.js
@@ -1,4 +1,4 @@
-export default gamepadController = (
+export default (
     gamepad, 
     //calbacks object with each function for each joystick direction, each function receive axis value
     joysticksCallbacks = {
@@ -24,20 +24,22 @@ export default gamepadController = (
         rpTopButton : (button)=>console.log('rpTopButton button pressed' + button),
         rpBottomButton : (button)=>console.log('rpBottomButton button pressed' + button),
         touchpadButton : (button)=>console.log('touchpadButton button pressed' + button),
+        leftActionButton : (button)=>console.log('leftActionButton button pressed' + button),
+        rightActionButton : (button)=>console.log('rightActionButton button pressed' + button),
     })=>{
 
     for (const [index, axis] of gamepad.axes.entries()) {
         switch (index) {
-            case 1: //left joystick horizontal axis
+            case 0: //left joystick horizontal axis
                 joysticksCallbacks.leftHorizontal(axis);
                 break;
-            case 2: //right joystick horizontal axis
+            case 1: //right joystick horizontal axis
                 joysticksCallbacks.leftVertical(axis);
                 break;
-            case 3: //left joystick vertical axis
+            case 2: //left joystick vertical axis
                 joysticksCallbacks.rightHorizontal(axis);
                 break;
-            case 4: //right joystick vertical axis
+            case 3: //right joystick vertical axis
                 joysticksCallbacks.rightVertical(axis);
                 break;
         }
@@ -45,49 +47,55 @@ export default gamepadController = (
      
     for (const [index, button] of gamepad.buttons.entries()) {
         switch (index) {
+            case 0: // button
+                buttonsCallbacks.rpBottomButton(button);
+                break;
             case 1: // button
-                buttonsCallbacks.leftArrowButton(button);
+                buttonsCallbacks.rpRightButton(button);
                 break;
             case 2: // button
-                buttonsCallbacks.rightArrowButton(button);
+                buttonsCallbacks.rpLeftButton(button);
                 break;
             case 3: // button
-                buttonsCallbacks.topArrowButton(button);
+                buttonsCallbacks.rpTopButton(button);
                 break;
             case 4: // button
-                buttonsCallbacks.bottomArrowButton(button);
+                buttonsCallbacks.L1Button(button);
                 break;
             case 5: // button
-                buttonsCallbacks.L1Button(button);
+                buttonsCallbacks.R1Button(button);
                 break;
             case 6: // button
                 buttonsCallbacks.L2Button(button);
                 break;
             case 7: // button
-                buttonsCallbacks.L3Button(button);
-                break;
-            case 8: // button
-                buttonsCallbacks.R1Button(button);
-                break;
-            case 9: // button
                 buttonsCallbacks.R2Button(button);
                 break;
+            case 8: // button
+                buttonsCallbacks.leftActionButton(button);
+                break;
+            case 9: // button
+                buttonsCallbacks.rightActionButton(button);
+                break;
             case 10: // button
-                buttonsCallbacks.R3Button(button);
+                buttonsCallbacks.L3Button(button);
                 break;
             case 11: // button
-                buttonsCallbacks.rpLeftButton(button);
+                buttonsCallbacks.R3Button(button);
                 break;
             case 12: // button
-                buttonsCallbacks.rpRightButton(button);
+                buttonsCallbacks.topArrowButton(button);
                 break;
             case 13: // button
-                buttonsCallbacks.rpTopButton(button);
+                buttonsCallbacks.bottomArrowButton(button);
                 break;
             case 14: // button
-                buttonsCallbacks.rpBottomButton(button);
+                buttonsCallbacks.leftArrowButton(button);
                 break;
             case 15: // button
+                buttonsCallbacks.rightActionButton(button);
+                break;
+            case 16: // button
                 buttonsCallbacks.touchpadButton(button);
                 break;
         }
